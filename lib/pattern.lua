b064ff958fb02bb48916c8a17fe256182528732f
Chris Lowis <chris.lowis@gmail.com>
2021-03-20 22:05:43 +0000

    Add pattern module
    I'm experimenting with extracting the pattern code into a (tested)
    module.

---
+++ lib/pattern.lua
@@ -0,0 +1,27 @@
+M = {}
+
+function M.new()
+  return {
+    pos = 1,
+    length = 16,
+    steps = {false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false}
+  }
+end
+
+function M.advance(pattern)
+  pattern.pos = pattern.pos + 1
+
+  if pattern.pos > pattern.length then
+    pattern.pos = 1
+  end
+
+  return pattern
+end
+
+function M.toggleStep(pattern, step)
+  pattern.steps[step] = not pattern.steps[step]
+
+  return pattern
+end
+
+return M
---
+++ spec/pattern_spec.lua
@@ -0,0 +1,36 @@
+describe('pattern', function()
+    setup(function()
+	pattern = require("lib/pattern")
+    end)
+
+    describe('new()', function()
+	it('should have an initial position', function()
+	    assert.same(1, pattern.new().pos)
+	end)
+    end)
+
+    describe('advance()', function()
+	it('should return a new pattern with position incremented by 1', function()
+	    assert.same(2, pattern.advance(pattern.new()).pos)
+	end)
+
+	it('should return to beginning when advanced past the pattern length', function()
+	    local p = pattern.new()
+
+	    for i = 1, 16 do
+	      p = pattern.advance(p)
+	    end
+	    assert.same(1, p.pos)
+	end)
+    end)
+
+    describe('toggleStep()', function()
+	it('sets the step to true if it is false and vice versa', function()
+	    local p = pattern.new()
+	    assert.same(false, p.steps[1])
+
+	    p = pattern.toggleStep(p, 16)
+	    assert.same(true, p.steps[16])
+	end)
+    end)
+end)
