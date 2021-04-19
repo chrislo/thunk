51fac5ea61a1513d41ed266af1d0d2726aefc274
Chris Lowis <chris.lowis@gmail.com>
2021-04-19 21:30:34 +0100

    Rename handle_press -> handle_short_press

--- lib/spec/controller_spec.lua
+++ lib/spec/controller_spec.lua
@@ -4,7 +4,7 @@ describe('controller', function()
         _ = require("luassert.match")._
     end)
 
-    describe('handle_press', function()
+    describe('handle_short_press', function()
         it("toggle steps for the selected track and page", function()
             local state = {
               pattern = Pattern.new(),
@@ -15,24 +15,24 @@ describe('controller', function()
             local s = spy.on(Pattern, "toggleStep")
 
             -- Step toggle on first row of step editor
-            Controller.handle_press(state, 1, 1)
+            Controller.handle_short_press(state, 1, 1)
             assert.spy(Pattern.toggleStep).was_called_with(_, 1, 1)
             Pattern.toggleStep:clear()
 
             -- Step toggle on second row of step editor
-            Controller.handle_press(state, 1, 2)
+            Controller.handle_short_press(state, 1, 2)
             assert.spy(Pattern.toggleStep).was_called_with(_, 9, 1)
             Pattern.toggleStep:clear()
 
             -- Step toggle on second page of step editor
             state.selected_page = {2, 1, 1, 1, 1, 1}
-            Controller.handle_press(state, 1, 1)
+            Controller.handle_short_press(state, 1, 1)
             assert.spy(Pattern.toggleStep).was_called_with(_, 17, 1)
             Pattern.toggleStep:clear()
 
             -- Step toggle on first page, second track of step editor
             state.selected_track = 2
-            Controller.handle_press(state, 1, 1)
+            Controller.handle_short_press(state, 1, 1)
             assert.spy(Pattern.toggleStep).was_called_with(_, 1, 2)
             Pattern.toggleStep:clear()
         end)
