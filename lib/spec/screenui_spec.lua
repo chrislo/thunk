ffe7d86e65c60e1a9214327dce9902477df48c7f
Chris Lowis <chris.lowis@gmail.com>
2021-06-10 21:58:41 +0100

    Add tests for screenui pattern mode

--- lib/spec/screenui_spec.lua
+++ lib/spec/screenui_spec.lua
@@ -14,44 +14,69 @@ describe('screenui', function()
 
             local state = State:new()
             state.selected_step = 1
-            state.edit_mode = 'step'
             state.sample_pool = sample_pool
-
-            entries = ScreenUI.menu_entries(state)
-            selected_step = state.pattern.tracks[state.selected_track].steps[state.selected_step]
         end)
 
-        it("displays the selected step sample", function()
-            local label = entries[1].label
-            assert.is_truthy(label:match("^sample.*foo$"))
+        describe('when the edit_mode is step', function()
+            before_each(function()
+                state.edit_mode = 'step'
+                entries = ScreenUI.menu_entries(state)
+                selected_step = state.pattern.tracks[state.selected_track].steps[state.selected_step]
+            end)
+
+            it("displays the selected step sample", function()
+                local label = entries[1].label
+                assert.is_truthy(label:match("^sample.*foo$"))
+            end)
+
+            it("allows us to change the sample", function()
+                assert.are.equal(nil, selected_step.sample_id)
+                entries[1].handler(1)
+                assert.are.equal(2, selected_step.sample_id)
+            end)
+
+            it("displays the selected step offset", function()
+                local label = entries[2].label
+                assert.is_truthy(label:match("^offset.*0$"))
+            end)
+
+            it("allows us to change the selected step offset", function()
+                assert.are.equal(0, selected_step.offset)
+                entries[2].handler(1)
+                assert.are.equal(1, selected_step.offset)
+            end)
+
+            it("displays the selected step velocity", function()
+                local label = entries[3].label
+                assert.is_truthy(label:match("^velocity.*127\.00$"))
+            end)
+
+            it("allows us to change the selected step velocity", function()
+                assert.are.equal(127, selected_step.velocity)
+                entries[3].handler(-1)
+                assert.are.equal(126, selected_step.velocity)
+            end)
         end)
 
-        it("allows us to change the sample #wip", function()
-            assert.are.equal(nil, selected_step.sample_id)
-            entries[1].handler(1)
-            assert.are.equal(2, selected_step.sample_id)
-        end)
+        describe('when the edit_mode is pattern', function()
+            _G.params = {
+              get = function(x) return 100 end
+            }
 
-        it("displays the selected step offset", function()
-            local label = entries[2].label
-            assert.is_truthy(label:match("^offset.*0$"))
-        end)
+            before_each(function()
+                state.edit_mode = 'pattern'
+                entries = ScreenUI.menu_entries(state)
+            end)
 
-        it("allows us to change the selected step offset", function()
-            assert.are.equal(0, selected_step.offset)
-            entries[2].handler(1)
-            assert.are.equal(1, selected_step.offset)
-        end)
+            it("displays the tempo", function()
+                local label = entries[1].label
+                assert.is_truthy(label:match("^tempo.*$"))
+            end)
 
-        it("displays the selected step velocity", function()
-            local label = entries[3].label
-            assert.is_truthy(label:match("^velocity.*127\.00$"))
-        end)
-
-        it("allows us to change the selected step velocity", function()
-            assert.are.equal(127, selected_step.velocity)
-            entries[3].handler(-1)
-            assert.are.equal(126, selected_step.velocity)
+            it("displays the swing", function()
+                local label = entries[2].label
+                assert.is_truthy(label:match("^swing.*$"))
+            end)
         end)
     end)
 end)
