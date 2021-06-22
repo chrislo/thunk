3ac452b74f1dac6f54edab4dd9b787f27689916d
Chris Lowis <chris.lowis@gmail.com>
2021-06-22 21:27:33 +0100

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+NORNS_IP=192.168.0.14
+
+rsync -azP . --exclude .git --delete norns:/home/we/dust/code/thunk
+echo 'norns.script.load("code/thunk/thunk.lua")' | websocat ws://$NORNS_IP:5555 -n1 --protocol bus.sp.nanomsg.org
