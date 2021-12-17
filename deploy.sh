b8b8f5f970a9984973671f4c45950d750c88a1cb
Chris Lowis <chris.lowis@gmail.com>
2021-12-17 09:22:17 -0500

    Exclude test.sc when deploying

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 NORNS_IP=192.168.0.14
 
-rsync -azP . --exclude .git --delete norns:/home/we/dust/code/thunk
+rsync -azP . --exclude .git --exclude test.sc --delete norns:/home/we/dust/code/thunk
 echo 'norns.script.load("code/thunk/thunk.lua")' | websocat ws://$NORNS_IP:5555 -n1 --protocol bus.sp.nanomsg.org
