429e8090e65c31e44159d5b6553b86e62f5574c1
Chris Lowis <chris.lowis@gmail.com>
2021-12-28 15:26:44 -0500

    Remove hardcoded NORNS_IP var
    This can be set on the command line when run
    
        NORNS_IP=192.168.2.2 ./deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,2 @@
-NORNS_IP=192.168.0.14
-
 rsync -azP . --exclude .git --exclude test.sc --delete norns:/home/we/dust/code/thunk
 echo 'norns.script.load("code/thunk/thunk.lua")' | websocat ws://$NORNS_IP:5555 -n1 --protocol bus.sp.nanomsg.org
